3dd6e9f2aed67e8e0e60cf5aa57959a2c700b875
vivieev <viviee1213@gmail.com>
2026-01-27 12:28:14 +0700

    feat: setup router app

---
+++ app/core/Router.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Core;
+
+
+class Router
+{
+
+    public function run(): variant_mod
+    {
+        
+    }
+
+}
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+require_once './app/core/Router.php';
+
+use App\Core\Router; 
+
+$router = new Router();
+$router->run();
+
+?>
